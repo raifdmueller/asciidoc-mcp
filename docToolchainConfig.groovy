1e8ed3bea87de6fe2bea959e9b9b58d9a0f9a470
Ralf D. Müller <ralf.d.mueller@gmail.com>
2025-10-05 10:04:19 +0200

    fix pdf build

--- docToolchainConfig.groovy
+++ docToolchainConfig.groovy
@@ -23,7 +23,7 @@ inputPath = 'docs';
 // pdfThemeDir = './src/docs/pdfTheme'
 
 inputFiles = [
-        //[file: 'doctoolchain_demo.adoc',       formats: ['html','pdf']],
+        [file: 'arc42-template.adoc',       formats: ['html','pdf']],
         //[file: 'arc42-template.adoc',    formats: ['html','pdf']],
         /** inputFiles **/
 ]
